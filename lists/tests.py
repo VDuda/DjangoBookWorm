c3af678cb2505f6cf0f77e70dc6e1c51d6804c93
Volodimir Duda <vduda@NuoDB.com>
2014-11-20 23:40:11 -0500

    Redirect after POST, and how all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -41,6 +41,37 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         home(request)
         self.assertEqual(Item.objects.count(), 0)
+    
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='Itemey 1')
+        Item.objects.create(text='Itemey 2')
+
+        request = HttpRequest()
+        response = home(request)
+
+        self.assertIn('Itemey 1', response.content.decode())
+        self.assertIn('Itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
